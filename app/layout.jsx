b3674d648a1840f85fe1055a445330b2247fe1bf
EnergizedAstronaut <174304106+EnergizedAstronaut@users.noreply.github.com>
2026-01-31 00:16:50 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 
+export const metadata = {
+  title: "MovieMatch",
+  description: "Discover movies you'll both love",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
